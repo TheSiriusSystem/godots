0b86a04d36e7c11125ee3c3103564c152a6eede3
TheSiriusSystem <91137432+TheSiriusSystem@users.noreply.github.com>
2024-01-27 20:01:52 +0600

    Implement PROJECT_CONFIG_FILENAMES and new version getters

--- src/utils.gd
+++ src/utils.gd
@@ -1,6 +1,12 @@
 class_name utils
 
 
+const PROJECT_CONFIG_FILENAMES: PackedStringArray = [
+	"project.godot",
+	"engine.cfg"
+]
+
+
 static func guess_editor_name(file_name: String):
 	var possible_editor_name = file_name.get_file()
 	var tokens_to_replace = []
@@ -30,7 +36,7 @@ static func find_project_godot_files(dir_path) -> Array[edir.DirListResult]:
 		ProjectSettings.globalize_path(dir_path), 
 		false,
 		(func(x: edir.DirListResult): 
-			return x.is_file and x.file == "project.godot"),
+			return x.is_file and PROJECT_CONFIG_FILENAMES.has(x.file)),
 		(func(x: String): 
 			return not x.get_file().begins_with("."))
 	)
@@ -67,3 +73,29 @@ static func disconnect_all(obj: Object):
 	for obj_signal in obj.get_signal_list():
 		for connection in obj.get_signal_connection_list(obj_signal.name):
 			obj.disconnect(obj_signal.name, connection.callable)
+
+
+static func extract_version_from_string(text: String, return_null_on_no_match: bool = false):
+	var version: PackedInt32Array = [0, 0, 0]
+	
+	var regex = RegEx.create_from_string("^(?:\\D+)?(?<major>\\d+)(?:\\.(?<minor>\\d+))?(?:\\.(?<patch>\\d+))?(?:.+?)?$")
+	var result = regex.search(text)
+	if result:
+		for group in range(result.get_group_count() + 1):
+			var component = result.get_string(group)
+			if component.is_valid_int():
+				version[group - 1] = int(component)
+	elif return_null_on_no_match:
+		return null
+	return version
+
+
+static func get_version_string(text: String) -> String:
+	var version = utils.extract_version_from_string(text, true)
+	if version:
+		var array: PackedStringArray = []
+		for version_number in version:
+			array.push_back(str(version_number))
+		return ".".join(array)
+	else:
+		return ""
